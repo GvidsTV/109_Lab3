0a247a78ca79d27a86b6bced369a3b65762d43cf
20Stacks G Code <20StacksGcode@Gs-MacBook-Pro.local>
2020-06-28 15:47:59 -0700

    commit change

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -6,7 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  message = "";
+  imageUrl = "";
 
   constructor() {}
+
+  post() {
+    console.log(this.message, this.imageUrl);
+  }
 
 }
